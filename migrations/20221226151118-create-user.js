8bad68372737bdc55a541d0802f567fb06fe32e1
yuvraj1902 <yuvraj@gkmit.co>
2022-12-26 20:55:22 +0530

    create user model

---
+++ migrations/20221226151118-create-user.js
@@ -0,0 +1,62 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()')
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        isAlpha: true
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        isAlpha: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        isAlphanumeric: true,
+      },
+      contact_no:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isNumeric:true
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+        defaultValue: null
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,55 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      isAlphanumeric: true,
+    },
+    contactNo:{
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isNumeric:true
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName:'user',
+    paranoid:true
+
+  });
+  return User;
+};
