dce2a5f38ed0b910176c01d03db19bcdf5e085e4
yuvraj1902 <yuvraj@gkmit.co>
2022-12-30 10:26:29 +0530

    changes in show and address routes files

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const User=require('./user.controller')
-const Event=require('./event.controller')
-const Show=require('./show.controller')
-const Address=require('./address.controller')
-module.exports={
-    User,Event,Show,Address
-}
--- validators/index.js
+++
@@ -1,7 +0,0 @@
-const userValidator=require('./user.validator')
-const eventValidator=require('./event.validator')
-const showValidator=require('./show.validator')
-const addressValidator=require('./address.validator')
-module.exports={
-    userValidator,eventValidator,showValidator,addressValidator
-}
