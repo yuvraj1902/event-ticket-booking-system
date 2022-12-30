a2a327ddbf1059752a248da27cfea76f3990606c
yuvraj1902 <yuvraj@gkmit.co>
2022-12-30 08:51:12 +0530

    change the name of mail file,booking and show-seat model, remove index.js from controllers

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,47 +1,47 @@
 const { Router } = require("express");
-const controllers = require("../controllers");
-const validators = require("../validators");
+const userController = require("../controllers/user.controller");
+const userValidator = require("../validators/user.validator");
 const genericResponse = require("../helper/generic-response.helper");
 const { checkRefreshToken, checkAccessToken } = require("../middlewares/auth");
 const router = Router();
 
 router.post(
   "/register",
-  validators.userValidator.registrationSchema,
-  controllers.User.registration,
+  userValidator.registrationSchema,
+  userController.registration,
   genericResponse.sendResponse
 );
 router.post(
   "/login",
-  validators.userValidator.loginSchema,
-  controllers.User.loginUser,
+  userValidator.loginSchema,
+  userController.loginUser,
   genericResponse.sendResponse
 );
 router.get(
   "/refresh-token",
   checkRefreshToken,
-  controllers.User.refreshToken,
+  userController.refreshToken,
   genericResponse.sendResponse
 );
 
 router.patch(
     "/reset-password",
     checkAccessToken,
-    validators.userValidator.resetPasswordSchema,
-    controllers.User.resetPassword,
+    userValidator.resetPasswordSchema,
+    userController.resetPassword,
     genericResponse.sendResponse
   );
 
   router.patch(
     "/forget-password",
-    validators.userValidator.forgetPasswordSchema,
-    controllers.User.forgetPassword,
+    userValidator.forgetPasswordSchema,
+    userController.forgetPassword,
     genericResponse.sendResponse
   );
   router.patch(
     "/reset-password/:id",
-    validators.userValidator.resetPasswordByLinkSchema,
-    controllers.User.resetPasswordByLink,
+    userValidator.resetPasswordByLinkSchema,
+    userController.resetPasswordByLink,
     genericResponse.sendResponse
   );
 
