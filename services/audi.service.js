9ab9bf48ceffbd3efb6793527ebb8f17eddf6cb7
yuvraj1902 <yuvraj@gkmit.co>
2023-01-01 19:34:51 +0530

    create audi serializer

--- services/audi.service.js
+++ services/audi.service.js
@@ -1,58 +1,55 @@
-const models=require("../models")
+const models = require("../models");
 const map = new Map();
 const createAudi = async (payload) => {
-    try {
-          const theatre = await models.Theatre.findOne({
-            where: { theatre_name: payload.theatreName },
-          });
-          
-          if (!theatre) {
-            throw new Error("Theatre does not exists");
-          }
+  try {
+    const theatre = await models.Theatre.findOne({
+      where: { theatre_name: payload.theatreName },
+    });
 
-          if(theatre.noOfScreens<map.get(theatre.id)){
-            throw new Error("Screen can't be assign to this theatre")
-          }
-
-          if(map.has(theatre.id)){
-              map.set(theatre.id,map.get(theatre.id)+1);
-          }else{
-              map.set(theatre.id,2)
-          }
-          const newPayload={
-            theatreId:theatre.id,
-            noOfSeats:payload.noOfSeats
-          }
-         await models.Audi.create(newPayload);
-        return "Audi has successfully created";
-      
-    } catch (error) {
-        return { data: null, error: error };
+    if (!theatre) {
+      throw new Error("Theatre does not exists");
     }
+
+    if (theatre.noOfScreens < map.get(theatre.id)) {
+      throw new Error("Screen can't be assign to this theatre");
+    }
+
+    if (map.has(theatre.id)) {
+      map.set(theatre.id, map.get(theatre.id) + 1);
+    } else {
+      map.set(theatre.id, 2);
+    }
+    const newPayload = {
+      theatreId: theatre.id,
+      noOfSeats: payload.noOfSeats,
+    };
+    await models.Audi.create(newPayload);
+    return "Audi has successfully created";
+  } catch (error) {
+    return { data: null, error: error };
+  }
 };
 
 const getAudi = async (payload) => {
-    
-    const theatre = await models.Theatre.findOne({
-        attributes: { exclude: ["deleted_at"] },
-        where:{
-            theatre_name:payload.theatreName
-        }
-      });
-      if(!theatre){
-        throw new Error("Theatre does not exists");
-    }
-    const audi = await models.Audi.findAll({
-        attributes: { exclude: ["deleted_at"] },
-        where:{
-            theatre_id:theatre.id
-        }
-      });
-    return audi;
+  const theatre = await models.Theatre.findOne({
+    attributes: { exclude: ["deleted_at"] },
+    where: {
+      theatre_name: payload.theatreName,
+    },
+  });
+  if (!theatre) {
+    throw new Error("Theatre does not exists");
   }
+  const audi = await models.Audi.findAll({
+    attributes: { exclude: ["deleted_at"] },
+    where: {
+      theatre_id: theatre.id,
+    },
+  });
+  return audi;
+};
 
-
-
-module.exports={
-    createAudi,getAudi
-}
+module.exports = {
+  createAudi,
+  getAudi,
+};
